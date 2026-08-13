7585893d8d465bb941997c5a86377edb59274b8b
UASenithu <snethviru@gmail.com>
2026-08-13 06:23:36 -0700

    Added Student Management, Password Reset, and Performance tracking

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -193,7 +193,7 @@ export default function LoginPage() {
           </motion.div>
 
 
-          // Add this after the password input field
+         
             <div className="text-right mt-1">
             <Link href="/reset-password" className="text-xs text-indigo-600 dark:text-indigo-400 hover:underline">
                 Forgot password?
